4f45c880f0010c4e8be5c42c60da8068e54e90b4
Alexander <skinnynpale@gmail.com>
2019-08-09 23:31:15 +0700

    Создал модуль dropdown.js и импортнул его в index.js и room.js

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -24,137 +24,14 @@ datepickerButtons[1].addEventListener("click", function(event) {
 });
 
 // Dropdown
-const input = document.getElementById('guests');
-const dropMenu = document.getElementById('guestsDrop');
-const values = document.querySelectorAll('.find-room .dropdown__value');
-const funcBtns = document.querySelectorAll('.dropdown__func-btn');
-const primeItem = document.getElementById('guestsDropPrimeItemValue'); // Младенцы
+window.input = document.getElementById('guests');
+window.dropMenu = document.getElementById('guestsDrop');
+window.values = document.querySelectorAll('.dropdown__content .dropdown__value');
+window.funcBtns = document.querySelectorAll('.dropdown__func-btn');
+window.primeItem = document.getElementById('guestsDropPrimeItemValue'); // Младенцы
 
-let amount = 0;
+window.amount = 0;
 
-// Анимация появления
-input.addEventListener("click", function(event) {
-  dropMenu.classList.toggle('dropdown__content_active');
-  input.classList.toggle('field_default_active');
-});
+const dep = require('../../components/field/dropdown/dropdown.js');
 
-
-// Закрытие dropdown__content_active если клик вне его
-$(document).mouseup(function (e){ // событие клика по веб-документу
-  if (dropMenu.classList.contains('dropdown__content_active')) {
-    var div = $("#guestsDrop"); // тут указываем ID элемента
-    if (!div.is(e.target) // если клик был не по нашему блоку
-        && div.has(e.target).length === 0) { // и не по его дочерним элементам
-          dropMenu.classList.toggle('dropdown__content_active');
-          input.classList.toggle('field_default_active');
-    }
-  }
-});
-
-// Расставление addEventListener
-for (var i = 0; i < values.length; i++) {
-  // Изначальная проверка на нули в значениях
-  if (values[i].innerHTML == 0) {
-    values[i].previousSibling.classList.toggle('dropdown__btn_disable');
-  }
-  // +
-  values[i].nextSibling.addEventListener("click", function(event) {
-    this.previousSibling.previousSibling.classList.remove('dropdown__btn_disable')
-    this.previousSibling.innerHTML = +this.previousSibling.innerHTML + 1;
-    amount += 1;
-    if (amount == 1) {
-      input.value = amount + ' гость';
-      funcBtns[0].style.opacity = "1";
-      funcBtns[0].style.cursor = "pointer";
-      if (primeItem.innerHTML > 0) {
-        input.value += ', ' + primeItem.innerHTML + ' младенец';
-      }
-    }
-    if (amount >= 2 && amount < 5) {
-      input.value = amount + ' гостя';
-      if (primeItem.innerHTML > 0) {
-        input.value += ', ' + primeItem.innerHTML + ' младенеца';
-      }
-    }
-    if (amount >= 5) {
-      input.value = amount + ' гостей';
-      if (primeItem.innerHTML >= 5) {
-        input.value += ', ' + primeItem.innerHTML + ' младенецев';
-      } else if (primeItem.innerHTML >= 2) {
-        input.value += ', ' + primeItem.innerHTML + ' младенеца';
-      } else if (primeItem.innerHTML == 1) {
-        input.value += ', ' + primeItem.innerHTML + ' младенец';
-      }
-    }
-  });
-  // -
-  values[i].previousSibling.addEventListener("click", function(event) {
-    if (this.nextSibling.innerHTML == 0) {
-      isZero(this.nextSibling);
-    }
-    else {
-      this.nextSibling.innerHTML = +this.nextSibling.innerHTML - 1;
-      isZero(this.nextSibling);
-      amount -= 1;
-      input.value = amount + ' гость';
-      if (amount == 0) {
-        input.value = 'Сколько гостей';
-        funcBtns[0].style.opacity = "0";
-        funcBtns[0].style.cursor = "default";
-      }
-      if (amount == 1) {
-        input.value = amount + ' гость';
-        if (primeItem.innerHTML > 0) {
-          input.value += ', ' + primeItem.innerHTML + ' младенец';
-        }
-      }
-      if (amount >= 2 && amount < 5) {
-        input.value = amount + ' гостя';
-        if (primeItem.innerHTML > 0) {
-          input.value += ', ' + primeItem.innerHTML + ' младенеца';
-        }
-      }
-      if (amount >= 5) {
-        input.value = amount + ' гостей';
-        if (primeItem.innerHTML >= 5) {
-          input.value += ', ' + primeItem.innerHTML + ' младенецев';
-        } else if (primeItem.innerHTML >= 2) {
-          input.value += ', ' + primeItem.innerHTML + ' младенеца';
-        } else if (primeItem.innerHTML == 1) {
-          input.value += ', ' + primeItem.innerHTML + ' младенец';
-        }
-      }
-    }
-  });
-}
-
-// Убрать кнопку Очистить если и так amount = 0
-if (amount == 0) {
-  funcBtns[0].style.opacity = "0";
-  funcBtns[0].style.cursor = "default";
-}
-
-// Проверка на ноль
-function isZero(e) {
-  if (e.innerHTML == 0) {
-    return !!e.previousSibling.classList.toggle('dropdown__btn_disable');
-  }
-}
-
-// Очистка значений
-funcBtns[0].addEventListener("click", function(event) {
-  for (var i = 0; i < values.length; i++) {
-    values[i].innerHTML = 0;
-    values[i].previousSibling.classList.add('dropdown__btn_disable');
-  }
-  amount = 0;
-  input.value = 'Сколько гостей';
-  funcBtns[0].style.opacity = "0";
-  funcBtns[0].style.cursor = "default";
-});
-
-// Применение значений
-funcBtns[1].addEventListener("click", function(event) {
-  dropMenu.classList.toggle('dropdown__content_active');
-  input.classList.toggle('field_default_active');
-});
+//
---
+++ app/pages/rooms/rooms.js
@@ -0,0 +1,46 @@
+// Datepicker
+$('#dateFilter').datepicker({
+  minDate: new Date(),
+  range: true,
+  multipleDatesSeparator: " - ",
+  prevHtml: '<div class="find-room__arrow-date find-room__arrow-date-prev"></div>',
+  nextHtml: '<div class="find-room__arrow-date find-room__arrow-date-next"></div>',
+  clearButton: true,
+  todayButton: true,
+  dateFormat: "dd M",
+});
+// 818 строка в datepicker.js для настроек
+
+// Закрытие календаря по нажатию кнопки применить
+const datepickerButtons = document.querySelectorAll('.datepicker--button');
+const myDatepicker = $('#dateFilter').datepicker().data('datepicker');
+
+$('#dateFilter').val('Выберите дату')
+
+datepickerButtons[1].addEventListener("click", function(event) {
+  myDatepicker.hide();
+});
+
+
+// Развернутый список с чекбоксами
+const moreComfortFilter = document.getElementById('moreComfortFilter');
+const moreComfortFilterWrapper = document.getElementById('moreComfortFilterWrapper');
+
+moreComfortFilter.addEventListener("click", function(event) {
+  moreComfortFilterWrapper.classList.toggle('checkbox__wrapper_expandable-active');
+  this.classList.toggle('checkbox__title_expandable-active');
+});
+
+
+// Dropdown
+window.input = document.getElementById('guestsFilter');
+window.dropMenu = document.getElementById('guestsDrop');
+window.values = document.querySelectorAll('.dropdown__content .dropdown__value');
+window.funcBtns = document.querySelectorAll('.dropdown__func-btn');
+window.primeItem = document.getElementById('guestsDropPrimeItemValue'); // Младенцы
+
+window.amount = 0;
+
+const dep = require('../../components/field/dropdown/dropdown.js');
+
+//
