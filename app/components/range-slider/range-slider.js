925d4b245188e0fde97939130e1ebfeac0dc5fd3
skinnynpale <skinnynpale@gmail.com>
2020-01-19 09:38:22 +0700

    - Removed useless destructuring #19 - Fixed BEM into colors-type #6

--- app/components/range-slider/range-slider.js
+++ app/components/range-slider/range-slider.js
@@ -2,13 +2,11 @@ function rangeSlider(anchorValues, options) {
   const valuesHTML = document.querySelector(anchorValues);
 
   $('.js-range-slider').ionRangeSlider({
-    ...{
-      onStart(data) {
-        valuesHTML.innerHTML = `${data.from}₽ - ${data.to}₽`;
-      },
-      onChange(data) {
-        valuesHTML.innerHTML = `${data.from}₽ - ${data.to}₽`;
-      },
+    onStart(data) {
+      valuesHTML.innerHTML = `${data.from}₽ - ${data.to}₽`;
+    },
+    onChange(data) {
+      valuesHTML.innerHTML = `${data.from}₽ - ${data.to}₽`;
     },
     ...options,
   });
