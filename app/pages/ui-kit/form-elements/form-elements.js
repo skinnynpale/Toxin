eae2eee2b065cb1405d01882174960e4d6a5b69b
skinnynpale <skinnynpale@gmail.com>
2020-01-21 13:21:26 +0700

    - Changed advantage => advantages - Fixed rate #6 - Fixed BEM into form-elements, cards, headers-footers #6 - Removed and separated user-form #6 - Upgrade sign-in and sign-up #6

--- app/pages/ui-kit/form-elements/form-elements.js
+++ app/pages/ui-kit/form-elements/form-elements.js
@@ -5,7 +5,51 @@ import Dropdown from '../../../components/dropdown/dropdown';
 import Calendar from '../../../components/calendar/calendar';
 import rangeSlider from '../../../components/range-slider/range-slider';
 
-// DropDown
+const DropdownWithoutButtons = new Dropdown({
+  anchor: document.getElementById('DropdownWithoutButtons'),
+  fallbackTitle: 'Выберите удобства',
+  showFullAmount: false,
+  items: [
+    {
+      title: 'CПАЛЬНИ',
+      show: true,
+      declensions: ['спальня', 'спальни', 'спален'],
+    },
+    {
+      title: 'КРОВАТИ',
+      show: true,
+      declensions: ['кровать', 'кровати', 'кроватей'],
+    },
+    {
+      title: 'ВАННЫЕ КОМНАТЫ',
+      show: true,
+      declensions: ['ванная комната', 'ванных комнаты', 'ванных комнат'],
+    },
+  ],
+  helpButtons: false,
+});
+
+const DropdownWithButtons = new Dropdown({
+  anchor: document.getElementById('DropdownWithButtons'),
+  fallbackTitle: 'Сколько гостей',
+  showFullAmount: true,
+  anchorDeclensions: ['гость', 'гостя', 'гостей'],
+  items: [
+    {
+      title: 'Младенцы',
+      show: true,
+      declensions: ['младенец', 'младенца', 'младенцев'],
+    },
+    {
+      title: 'Дети',
+    },
+    {
+      title: 'Взрослые',
+    },
+  ],
+  helpButtons: true,
+});
+
 const guestsDropDown = new Dropdown({
   anchor: document.getElementById('elements-guestsDropDown'),
   fallbackTitle: 'Сколько гостей',
@@ -27,7 +71,6 @@ const guestsDropDown = new Dropdown({
   helpButtons: true,
 });
 
-// Datepicker
 const arrivalCalendar = new Calendar('#formElementsCalendar', {
   multipleDatesSeparator: '-',
   onSelect(formattedDate) {
@@ -37,14 +80,12 @@ const arrivalCalendar = new Calendar('#formElementsCalendar', {
 });
 arrivalCalendar.calendar.css({ top: `${80}px` });
 
-// Calendar
 const dateFilterCalendar = new Calendar('#filterDate', {
   dateFormat: 'dd M',
   multipleDatesSeparator: ' - ',
 });
-dateFilterCalendar.calendar.css({ left: `${290}px`, width: '318px' });
+dateFilterCalendar.calendar.css({ left: `${350}px`, width: '318px' });
 
-// rangeslider
 const rangePrices = rangeSlider('.form-elements__slider-values', {
   type: 'double',
   min: 0,
@@ -53,29 +94,4 @@ const rangePrices = rangeSlider('.form-elements__slider-values', {
   to: 10000,
   hide_min_max: true,
   hide_from_to: true,
-});
-
-// Dropdown
-const comfortFilterDropDown = new Dropdown({
-  anchor: document.getElementById('comfortFilter'),
-  fallbackTitle: 'Выберите удобства',
-  showFullAmount: false,
-  items: [
-    {
-      title: 'CПАЛЬНИ',
-      show: true,
-      declensions: ['спальня', 'спальни', 'спален'],
-    },
-    {
-      title: 'КРОВАТИ',
-      show: true,
-      declensions: ['кровать', 'кровати', 'кроватей'],
-    },
-    {
-      title: 'ВАННЫЕ КОМНАТЫ',
-      show: true,
-      declensions: ['ванная комната', 'ванных комнаты', 'ванных комнат'],
-    },
-  ],
-  helpButtons: false,
 });
