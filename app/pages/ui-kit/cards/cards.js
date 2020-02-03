f4810f2c5a16a52a0d23e07245b2be79b1a93418
skinnynpale <skinnynpale@gmail.com>
2020-02-03 18:32:58 +0700

    - Minor fixes #6

--- app/pages/ui-kit/cards/cards.js
+++ app/pages/ui-kit/cards/cards.js
@@ -1,6 +1,6 @@
 // Datepicker
 import Calendar from '../../../components/calendar/calendar';
 
-const cardsCalendar = new Calendar('#ui-kit__preview-calendar', {}, true);
+const cardsCalendar = new Calendar('#cards__preview-calendar', {}, true);
 cardsCalendar.calendar.css({ left: 0, position: 'static', width: '318px' });
 cardsCalendar.showCalendar();
